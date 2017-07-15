779cec7541cb533ae7cd36137aef691452c9d3a5
Monbedon <monbedon@gmail.com>
2017-07-14 22:36:03 -0700

    stopped at liri.js

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<87hOsdk06ZsnfIbOLhMMlou5x>',
+  consumer_secret: '<Xcis2GLcMEP8j14LtfIGw789e7Q8821qmmS87dTob0htaj1RNH>',
+  access_token_key: '<885883179675955200-yb7ZAKqhxWr68jhevwOlN1PEgRrflzx>',
+  access_token_secret: '<FJKzdSUx5EpeCVDqpCqQmOmoMr8JwexTyNdDsv1fsgUmR>',
+}
