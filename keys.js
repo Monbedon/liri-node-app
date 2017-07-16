0da3acc40d83c616ac3e0cac3efcecae856a2cd2
Monbedon <monbedon@gmail.com>
2017-07-16 13:25:08 -0700

    working on it

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<08wyXK1ul7sFyMhsR8v8gQlL6>',
-  consumer_secret: '<cbyfnaz76Z6tSb3Vna3awiEw6PKXlte9egFXO9kA3KE15aJhnJ>',
-  access_token_key: '<885883179675955200-4kwKmwn9ijZFhFNF4w1HKrFR5RQyaAT>',
+  consumer_key: '<rhmaYrGKdTvzTzU3ns7K8woFq>',
+  consumer_secret: '<Z1gsR0fJeJJfpOSVKs8xHBaLjiiv1t1yr0HZZLmjfi2L1AAXD4>',
+  access_token: '<885883179675955200-4kwKmwn9ijZFhFNF4w1HKrFR5RQyaAT>',
   access_token_secret: '<UTUGXXJ2m40MmBvpaL7ZPexvzoZfxY7V2ZjZ6ys51g3Yy>',
-}
+};
--- liri.js
+++ liri.js
@@ -1,40 +1,103 @@
-console.log("working");
-// Grabs the twitter data from keys
 
+// Grabs the npm libraries
+	
 var Twitter = require('twitter');
-var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var request = require('request');
 
-// var Spotify = require('node-spotify-api');
-// var request = require('request');
-
-
-////////////////////////////////////
-
-var myTweets = new Twitter(keys);
-
-var params = {from:'MyTweetsForNow', count: 3
-};
+// Connects the access to my twitter account
+ var keys = require("./keys.js");
 
 
-myTweets.get('statuses/user_timeline', params, gotData); 
+//access to file system//
 
-function gotData(error, data, response){
-	var tweets =data.statuses;
-	for (var i= 0; i < tweets.length; i++)
-		console.log(tweets[i].text);
+var fs = require('fs');
 
-// 	if(!error){
-// 		console.log(tweets);
-// 	}
-// 	else{
-// 		throw error
-	};
-// });
+//////////////////////////////////////////////////////
 
 
+var twitterKeys= keys.twitterKeys;
+
+ //for switching, creating variable
+var command = process.argv[2];
+
+// for grabbing variable
+var commandArgument = process.argv[3];
+
+//switch
+
+switch(command){
+	case 'my-tweets':
+	myTweets();
+	break;
+
+	case 'spotify-this-song':
+	mySpotify(commandArgument);
+	break;
+
+	case 'movie-this':
+	movieThis(commandArgument);
+	break;
+
+	case 'do-what-it-says':
+		doWhatItSays();
+		break; 
+
+	default:
+		console.log("Not Valid")
+}
+
+function myTweets(){
+	var client = new Twitter(keys);
 
 
-// request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&?apikey=40e9cece"), function(error, response, body) {
+client.get('statuses/user_timeline', {count: 20, trim_user: false}, 
+	function(error, tweets, response) { 
+		if (error) return console.log('Twitter error: ' + error);
+		for (var i=0; i<tweets.length; i++) {
+			logThis(tweets[i].created_at);
+			logThis(tweets[i].text);
+		}
+	});
+}
+
+
+// // //////////////////////////////////////////
+
+function mySpotify(){
+	var spotify = new Spotify({
+  		client_id: '<668006378bda4d27acad106ac420be9d>',
+  		client_secret: '<ba7216a09ba34eb28a09ddc6b1847bdd>'
+	});
+
+spotify.search({ type: 'track', query: 'keyword'|| 'The Sign Ace of Base' }, 
+	function(err, data) {
+  	if (err) {
+    	return console.log('Spotify Error occurred: ' + err);
+  }
+ 
+  	if (data.tracks.items.length > 0) {
+  		var record = data.tracks.items[0];
+ 
+        console.log(' ');
+        console.log('================ Song Info ================');
+        console.log('Artist: ' + record.artists[0].name);
+        console.log('Name: ' + record.name);
+        console.log('Link: ' + record.preview_url);
+        console.log('Album: ' + record.album.name);
+        console.log('===========================================');
+        console.log(' ');
+
+        app.logData(data);
+      } else {
+        console.log('No song data found.');
+      }
+
+    });
+  }
+//////////////////////////////////////////////////////
+
+// request("http://www.omdbapi.com/?t=&y=&plot=short&?apikey=40e9cece"), function(error, response, body) {
 
 //   // If the request was successful
 //   if (!error && response.statusCode === 200) {
@@ -44,4 +107,3 @@ function gotData(error, data, response){
 //     console.log('error:', error);
 //   }
 // };
-
