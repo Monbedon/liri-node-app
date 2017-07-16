7c96642006ef3bd7195a3074934dd4b2e6e09a3a
Monbedon <monbedon@gmail.com>
2017-07-15 18:49:04 -0700

    package.json

---
+++ liri.js
@@ -0,0 +1,15 @@
+
+// Grabs the twitter data from keys
+var twitter = require("./keys.js");
+// var Spotify = require('node-spotify-api');
+// var request = require('request');
+
+
+var my-tweets = twitter.twitterKeys;
+
+// for(var key in my-tweets){
+// 	console.log(process.argv);
+// }
+var keys = Object.keys(my-tweets);
+
+
